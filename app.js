68752609495de6a4cd9c88ddf4f38fa5f31e718c
hbarrett100 <55139243+hbarrett100@users.noreply.github.com>
2020-05-21 11:01:23 +0100

    add event listener for rolling dice

--- app.js
+++ app.js
@@ -8,3 +8,42 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML =  '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+//faster than query selector when we have ID
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//anonymous function - can only be used in this context
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+
+    //change image based on random number rolled
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+
+})
